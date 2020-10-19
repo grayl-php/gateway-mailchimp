695b7abf5e017a02b51b213e14193c2d084036f4
Grogwood LLC <dev@grogwood.com>
2020-10-19 14:09:52 -0700

    Code formatting

--- src/MailChimpPorter.php
+++ src/MailChimpPorter.php
@@ -1,153 +1,139 @@
 <?php
 
-namespace Grayl\Gateway\MailChimp;
+   namespace Grayl\Gateway\MailChimp;
 
-use DrewM\MailChimp\MailChimp;
-use Grayl\Gateway\Common\GatewayPorterAbstract;
-use Grayl\Gateway\MailChimp\Controller\MailChimpSubscribeRequestController;
-use Grayl\Gateway\MailChimp\Entity\MailChimpGatewayData;
-use Grayl\Gateway\MailChimp\Entity\MailChimpSubscribeRequestData;
-use Grayl\Gateway\MailChimp\Service\MailChimpGatewayService;
-use Grayl\Gateway\MailChimp\Service\MailChimpSubscribeRequestService;
-use Grayl\Gateway\MailChimp\Service\MailChimpSubscribeResponseService;
-use Grayl\Mixin\Common\Traits\StaticTrait;
+   use DrewM\MailChimp\MailChimp;
+   use Grayl\Gateway\Common\GatewayPorterAbstract;
+   use Grayl\Gateway\MailChimp\Controller\MailChimpSubscribeRequestController;
+   use Grayl\Gateway\MailChimp\Entity\MailChimpGatewayData;
+   use Grayl\Gateway\MailChimp\Entity\MailChimpSubscribeRequestData;
+   use Grayl\Gateway\MailChimp\Service\MailChimpGatewayService;
+   use Grayl\Gateway\MailChimp\Service\MailChimpSubscribeRequestService;
+   use Grayl\Gateway\MailChimp\Service\MailChimpSubscribeResponseService;
+   use Grayl\Mixin\Common\Traits\StaticTrait;
 
-/**
- * Front-end for the MailChimp package
- * @method MailChimpGatewayData getSavedGatewayDataEntity (string $endpoint_id)
- *
- * @package Grayl\Gateway\MailChimp
- */
-class MailChimpPorter extends
-    GatewayPorterAbstract
-{
+   /**
+    * Front-end for the MailChimp package
+    * @method MailChimpGatewayData getSavedGatewayDataEntity ( string $endpoint_id )
+    *
+    * @package Grayl\Gateway\MailChimp
+    */
+   class MailChimpPorter extends GatewayPorterAbstract
+   {
 
-    // Use the static instance trait
-    use StaticTrait;
+      // Use the static instance trait
+      use StaticTrait;
 
-    /**
-     * The name of the config file for the MailChimp package
-     *
-     * @var string
-     */
-    protected string $config_file = 'gateway.mailchimp.php';
-
-
-    /**
-     * Creates a new MailChimp object for use in a MailChimpGatewayData entity
-     *
-     * @param array $credentials An array containing all of the credentials needed to create the gateway API
-     *
-     * @return MailChimp
-     * @throws \Exception
-     */
-    public function newGatewayAPI(array $credentials): object
-    {
-
-        // Return the new API entity
-        return new MailChimp($credentials['token']);
-    }
+      /**
+       * The name of the config file for the MailChimp package
+       *
+       * @var string
+       */
+      protected string $config_file = 'gateway.mailchimp.php';
 
 
-    /**
-     * Creates a new MailChimpGatewayData entity
-     *
-     * @param string $endpoint_id The API endpoint ID to use (typically "default" is there is only one API gateway)
-     *
-     * @return MailChimpGatewayData
-     * @throws \Exception
-     */
-    public function newGatewayDataEntity(string $endpoint_id): object
-    {
+      /**
+       * Creates a new MailChimp object for use in a MailChimpGatewayData entity
+       *
+       * @param array $credentials An array containing all of the credentials needed to create the gateway API
+       *
+       * @return MailChimp
+       * @throws \Exception
+       */
+      public function newGatewayAPI ( array $credentials ): object
+      {
 
-        // Grab the gateway service
-        $service = new MailChimpGatewayService();
-
-        // Get an API
-        $api = $this->newGatewayAPI(
-            $service->getAPICredentials(
-                $this->config,
-                $this->environment,
-                $endpoint_id
-            )
-        );
-
-        // Configure the API as needed using the service
-        $service->configureAPI(
-            $api,
-            $this->environment
-        );
-
-        // Return the gateway
-        return new MailChimpGatewayData(
-            $api,
-            $this->config->getConfig('name'),
-            $this->environment
-        );
-    }
+         // Return the new API entity
+         return new MailChimp( $credentials[ 'token' ] );
+      }
 
 
-    /**
-     * Creates a new MailChimpSubscribeRequestController entity
-     *
-     * @param string $list_alias    The MailChimp list alias from the Config to add this subscriber to
-     * @param string $email_address The email address of the subscriber
-     * @param string $status        The default status of the subscriber
-     * @param array  $merge_tags    The associative array of merge tags to set ( key => value )
-     *
-     * @return MailChimpSubscribeRequestController
-     * @throws \Exception
-     */
-    public function newMailChimpSubscribeRequestController(
-        string $list_alias,
-        string $email_address,
-        string $status,
-        array $merge_tags
-    ): MailChimpSubscribeRequestController {
+      /**
+       * Creates a new MailChimpGatewayData entity
+       *
+       * @param string $endpoint_id The API endpoint ID to use (typically "default" is there is only one API gateway)
+       *
+       * @return MailChimpGatewayData
+       * @throws \Exception
+       */
+      public function newGatewayDataEntity ( string $endpoint_id ): object
+      {
 
-        // Convert the list name into a list ID
-        $list_id = $this->getMailChimpListIDFromListAlias($list_alias);
+         // Grab the gateway service
+         $service = new MailChimpGatewayService();
 
-        // Create the MailChimpSubscribeRequestData entity
-        $request_data = new MailChimpSubscribeRequestData(
-            'subscribe',
-            $list_id,
-            $email_address,
-            $status,
-            $merge_tags
-        );
+         // Get an API
+         $api = $this->newGatewayAPI( $service->getAPICredentials( $this->config,
+                                                                   $this->environment,
+                                                                   $endpoint_id ) );
 
-        // Return a new MailChimpSubscribeRequestController entity
-        return new MailChimpSubscribeRequestController(
-            $this->getSavedGatewayDataEntity('default'),
-            $request_data,
-            new MailChimpSubscribeRequestService(),
-            new MailChimpSubscribeResponseService()
-        );
-    }
+         // Configure the API as needed using the service
+         $service->configureAPI( $api,
+                                 $this->environment );
+
+         // Return the gateway
+         return new MailChimpGatewayData( $api,
+                                          $this->config->getConfig( 'name' ),
+                                          $this->environment );
+      }
 
 
-    /**
-     * Gets the actual MailChimp list ID using a list alias from the Config
-     *
-     * @param string $list_alias The MailChimp list alias from the Config
-     *
-     * @return ?string
-     */
-    private function getMailChimpListIDFromListAlias(string $list_alias
-    ): ?string {
+      /**
+       * Creates a new MailChimpSubscribeRequestController entity
+       *
+       * @param string $list_alias    The MailChimp list alias from the Config to add this subscriber to
+       * @param string $email_address The email address of the subscriber
+       * @param string $status        The default status of the subscriber
+       * @param array  $merge_tags    The associative array of merge tags to set ( key => value )
+       *
+       * @return MailChimpSubscribeRequestController
+       * @throws \Exception
+       */
+      public function newMailChimpSubscribeRequestController ( string $list_alias,
+                                                               string $email_address,
+                                                               string $status,
+                                                               array $merge_tags ): MailChimpSubscribeRequestController
+      {
 
-        // Get the full array of lists
-        $lists = $this->config->getConfig('lists');
+         // Convert the list name into a list ID
+         $list_id = $this->getMailChimpListIDFromListAlias( $list_alias );
 
-        // Return the value if we have one
-        if ( ! empty ($lists[$list_alias])) {
+         // Create the MailChimpSubscribeRequestData entity
+         $request_data = new MailChimpSubscribeRequestData( 'subscribe',
+                                                            $list_id,
+                                                            $email_address,
+                                                            $status,
+                                                            $merge_tags );
+
+         // Return a new MailChimpSubscribeRequestController entity
+         return new MailChimpSubscribeRequestController( $this->getSavedGatewayDataEntity( 'default' ),
+                                                         $request_data,
+                                                         new MailChimpSubscribeRequestService(),
+                                                         new MailChimpSubscribeResponseService() );
+      }
+
+
+      /**
+       * Gets the actual MailChimp list ID using a list alias from the Config
+       *
+       * @param string $list_alias The MailChimp list alias from the Config
+       *
+       * @return ?string
+       */
+      private function getMailChimpListIDFromListAlias ( string $list_alias ): ?string
+      {
+
+         // Get the full array of lists
+         $lists = $this->config->getConfig( 'lists' );
+
+         // Return the value if we have one
+         if ( ! empty ( $lists[ $list_alias ] ) ) {
             // Match
-            return $lists[$list_alias];
-        }
+            return $lists[ $list_alias ];
+         }
 
-        // No match
-        return null;
-    }
+         // No match
+         return null;
+      }
 
-}
+   }
--- tests/MailChimpSubscribeRequestControllerTest.php
+++ tests/MailChimpSubscribeRequestControllerTest.php
@@ -1,137 +1,121 @@
 <?php
 
-namespace Grayl\Test\Gateway\MailChimp;
+   namespace Grayl\Test\Gateway\MailChimp;
 
-use Grayl\Gateway\MailChimp\Controller\MailChimpSubscribeRequestController;
-use Grayl\Gateway\MailChimp\Controller\MailChimpSubscribeResponseController;
-use Grayl\Gateway\MailChimp\Entity\MailChimpGatewayData;
-use Grayl\Gateway\MailChimp\MailChimpPorter;
-use PHPUnit\Framework\TestCase;
+   use Grayl\Gateway\MailChimp\Controller\MailChimpSubscribeRequestController;
+   use Grayl\Gateway\MailChimp\Controller\MailChimpSubscribeResponseController;
+   use Grayl\Gateway\MailChimp\Entity\MailChimpGatewayData;
+   use Grayl\Gateway\MailChimp\MailChimpPorter;
+   use PHPUnit\Framework\TestCase;
 
-/**
- * Test class for the MailChimp package
- *
- * @package Grayl\Gateway\MailChimp
- */
-class MailChimpSubscribeRequestControllerTest extends
-    TestCase
-{
+   /**
+    * Test class for the MailChimp package
+    *
+    * @package Grayl\Gateway\MailChimp
+    */
+   class MailChimpSubscribeRequestControllerTest extends TestCase
+   {
 
-    /**
-     * Test setup for sandbox environment
-     */
-    public static function setUpBeforeClass(): void
-    {
+      /**
+       * Test setup for sandbox environment
+       */
+      public static function setUpBeforeClass (): void
+      {
 
-        // Change the API environment to sandbox mode
-        MailChimpPorter::getInstance()
-            ->setEnvironment('sandbox');
-    }
-
-
-    /**
-     * Tests the creation of a MailChimpGatewayData object
-     *
-     * @return MailChimpGatewayData
-     * @throws \Exception
-     */
-    public function testCreateMailChimpGatewayData(): MailChimpGatewayData
-    {
-
-        // Create the object
-        $gateway = MailChimpPorter::getInstance()
-            ->getSavedGatewayDataEntity(
-                'default'
-            );
-
-        // Check the type of object returned
-        $this->assertInstanceOf(
-            MailChimpGatewayData::class,
-            $gateway
-        );
-
-        // Return the object
-        return $gateway;
-    }
+         // Change the API environment to sandbox mode
+         MailChimpPorter::getInstance()
+                        ->setEnvironment( 'sandbox' );
+      }
 
 
-    /**
-     * Tests the creation of a MailChimpSubscribeRequestController object
-     *
-     * @return MailChimpSubscribeRequestController
-     * @throws \Exception
-     */
-    public function testCreateMailChimpSubscribeRequestController(
-    ): MailChimpSubscribeRequestController
-    {
+      /**
+       * Tests the creation of a MailChimpGatewayData object
+       *
+       * @return MailChimpGatewayData
+       * @throws \Exception
+       */
+      public function testCreateMailChimpGatewayData (): MailChimpGatewayData
+      {
 
-        // Create the object
-        $request = MailChimpPorter::getInstance()
-            ->newMailChimpSubscribeRequestController(
-                'newsletter',
-                'devtest@grogwood.com',
-                'pending',
-                [
-                    'FNAME' => 'TEST',
-                    'LNAME' => 'TESTING',
-                ]
-            );
+         // Create the object
+         $gateway = MailChimpPorter::getInstance()
+                                   ->getSavedGatewayDataEntity( 'default' );
 
-        // Check the type of object returned
-        $this->assertInstanceOf(
-            MailChimpSubscribeRequestController::class,
-            $request
-        );
+         // Check the type of object returned
+         $this->assertInstanceOf( MailChimpGatewayData::class,
+                                  $gateway );
 
-        // Return the object
-        return $request;
-    }
+         // Return the object
+         return $gateway;
+      }
 
 
-    /**
-     * Tests the sending of a MailChimpSubscribeRequestData through a MailChimpSubscribeRequestController
-     *
-     * @param MailChimpSubscribeRequestController $request A configured MailChimpSubscribeRequestController entity to use as a gateway
-     *
-     * @depends testCreateMailChimpSubscribeRequestController
-     * @return MailChimpSubscribeResponseController
-     * @throws \Exception
-     */
-    public function testSendMailChimpSubscribeRequestController(
-        MailChimpSubscribeRequestController $request
-    ): MailChimpSubscribeResponseController {
+      /**
+       * Tests the creation of a MailChimpSubscribeRequestController object
+       *
+       * @return MailChimpSubscribeRequestController
+       * @throws \Exception
+       */
+      public function testCreateMailChimpSubscribeRequestController (): MailChimpSubscribeRequestController
+      {
 
-        // Send the request using the gateway
-        $response = $request->sendRequest();
+         // Create the object
+         $request = MailChimpPorter::getInstance()
+                                   ->newMailChimpSubscribeRequestController( 'newsletter',
+                                                                             'devtest@grogwood.com',
+                                                                             'pending',
+                                                                             [ 'FNAME' => 'TEST',
+                                                                               'LNAME' => 'TESTING', ] );
 
-        // Check the type of object returned
-        $this->assertInstanceOf(
-            MailChimpSubscribeResponseController::class,
-            $response
-        );
+         // Check the type of object returned
+         $this->assertInstanceOf( MailChimpSubscribeRequestController::class,
+                                  $request );
 
-        // Return the response
-        return $response;
-    }
+         // Return the object
+         return $request;
+      }
 
 
-    /**
-     * Checks a MailChimpSubscribeResponseController for data and errors
-     *
-     * @param MailChimpSubscribeResponseController $response A MailChimpSubscribeResponseController returned from the gateway
-     *
-     * @depends testSendMailChimpSubscribeRequestController
-     */
-    public function testMailChimpSubscribeResponseController(
-        MailChimpSubscribeResponseController $response
-    ): void {
+      /**
+       * Tests the sending of a MailChimpSubscribeRequestData through a MailChimpSubscribeRequestController
+       *
+       * @param MailChimpSubscribeRequestController $request A configured MailChimpSubscribeRequestController entity to use as a gateway
+       *
+       * @depends testCreateMailChimpSubscribeRequestController
+       * @return MailChimpSubscribeResponseController
+       * @throws \Exception
+       */
+      public function testSendMailChimpSubscribeRequestController ( MailChimpSubscribeRequestController $request ): MailChimpSubscribeResponseController
+      {
 
-        // Test the data
-        $this->assertTrue($response->isSuccessful());
-        $this->assertNotNull($response->getReferenceID());
+         // Send the request using the gateway
+         $response = $request->sendRequest();
 
-        // Test the raw data
-        $this->assertIsArray($response->getData());
-    }
+         // Check the type of object returned
+         $this->assertInstanceOf( MailChimpSubscribeResponseController::class,
+                                  $response );
 
-}
+         // Return the response
+         return $response;
+      }
+
+
+      /**
+       * Checks a MailChimpSubscribeResponseController for data and errors
+       *
+       * @param MailChimpSubscribeResponseController $response A MailChimpSubscribeResponseController returned from the gateway
+       *
+       * @depends testSendMailChimpSubscribeRequestController
+       */
+      public function testMailChimpSubscribeResponseController ( MailChimpSubscribeResponseController $response ): void
+      {
+
+         // Test the data
+         $this->assertTrue( $response->isSuccessful() );
+         $this->assertNotNull( $response->getReferenceID() );
+
+         // Test the raw data
+         $this->assertIsArray( $response->getData() );
+      }
+
+   }
